1daf85bba38bfb39bfdf79b01a4797ec7fc785dd
RobertGarabetian <robertgarabetian90@gmail.com>
2024-06-04 13:25:32 -0700

    first commit

---
+++ cloud-nine-event-rentals/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Hero from './components/Hero'
+import MidSection from './components/MidSection'
+
+function App() {
+
+  return (
+    <>
+      <Hero/>
+      <MidSection/>
+    </>
+  )
+}
+
+export default App
---
+++ cloud-nine-event-rentals/src/components/MidSection.jsx
@@ -0,0 +1,9 @@
+const MidSection = () => {
+  return (
+    <div className="w-screen h-screen bg-amber-400">
+
+    </div>
+  )
+}
+
+export default MidSection
