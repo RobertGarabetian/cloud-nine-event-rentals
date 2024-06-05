5ef23fb411c923439e70177a9cd9dd3928665a9f
RobertGarabetian <robertgarabetian90@gmail.com>
2024-06-05 11:35:34 -0700

    Pictures and stuff

--- cloud-nine-event-rentals/src/components/Hero.jsx
+++ cloud-nine-event-rentals/src/components/Hero.jsx
@@ -1,49 +1,24 @@
-import { useRef } from "react";
-import {
-  motion,
-  useScroll,
-  useSpring,
-  useTransform,
-} from "framer-motion";
-
-function useParallax(value, distance) {
-    return useTransform(value, [0, 1], [-distance, distance]);
-}
-
-const Picture= (id) => {
-    const ref = useRef(null);
-    const { scrollYProgress } = useScroll({ target: ref });
-    const y = useParallax(scrollYProgress, 300);
-  
-    return (
-      <section>
-        <div ref={ref}>
-          <img src={`/${id}.jpg`} alt="A London skyscraper" />
-        </div>
-        <motion.h2 style={{ y }}>{`#00${id}`}</motion.h2>
-      </section>
-    );
-  }
-  
 const Hero = () => {
-    const { scrollYProgress } = useScroll();
-    const scaleX = useSpring(scrollYProgress, {
-      stiffness: 100,
-      damping: 30,
-      restDelta: 0.001
-    });
-  
-    return (
-      <>
-        {[1, 2, 3, 4, 5].map((image) => 
-        (
-          <Picture id={image} key={image} />
-        )
-        )}
-        <motion.div className="progress" style={{ scaleX }} />
-      </>
-    )
-}
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage:
+          "url(4.jpg)",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-10"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="text-white mb-5 text-7xl font-bold">Cloud Nine Event Rentals</h1>
+          <h2 className="mb-5 text-slate-200 text-xl font-semibold ">
+            Make Your Memories Last a Lifetime!
+          </h2>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// FOR THIS TO WORK, I HAVE TO HAVE 5 IMAGES IN PUBLIC DIRECTORY NAMED 1.jpg, 2.jpg, ...
-export default Hero
+export default Hero;
