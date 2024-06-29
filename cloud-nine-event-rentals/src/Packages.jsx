c4aeefc31151d094743c82211b5fb173f4569022
RobertGarabetian <robertgarabetian90@gmail.com>
2024-06-28 18:13:34 -0700

    Done with packages page for now...

--- cloud-nine-event-rentals/src/Packages.jsx
+++ cloud-nine-event-rentals/src/Packages.jsx
@@ -11,15 +11,28 @@ const Packages = () => {
       <div className="flex flex-col w-full bg-white items-center">
         <PackCards />
       </div>
-      <div className="flex flex-col w-full bg-slate-50 items-center p-14">
-        <h3 className="text-5xl text-slate-500 text-center">Add Ons</h3>
+      <div className="flex flex-col w-full bg-slate-50 items-center p-14 h-auto">
+        <h3 className="text-5xl text-indigo-600 text-center">Add Ons</h3>
         <div className="flex justify-center">
           <div className="h-16 " />
           <div className="divider divider-horizontal divider-primary flex justify-center"></div>
           <div className="h-16 " />
         </div>
-        <div className="h-auto w-3/5 bg-transparent border-r-2 border-t-2 border-indigo-500" />
-        
+        <div className="h-auto w-1/2 bg-transparent border-r-2 border-t-2 border-indigo-500" />
+        <div className="w-full flex flex-row justify-around">
+          <div className="w-1/3 h-40 bg-transparent text-black text-center flex flex-col pt-8">
+            <h3 className="text-3xl text-indigo-500">Inflatable Tent</h3>
+            <p className="text-slate-500 text-md mt-auto">
+              $100/day
+            </p>
+          </div>
+          <div className="w-1/3 h-40 bg-transparent text-black text-center pt-8 flex flex-col">
+            <h3 className="text-3xl text-indigo-500">Custom Props <br />and Backdrops</h3>
+            <p className="text-slate-500 text-md mt-auto">
+              contact for price estimate
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
